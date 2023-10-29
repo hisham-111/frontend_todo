5a059b5408c2d77603efbd5b9361036ce54d2dc9
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2023-10-29 15:46:45 +0200

    fix the notFound page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/HomeScreenComponents/home.jsx";
 import ProtectedRoute from "./components/utils/protectedRoute.js";
 import Unauthorized from "./pages/Unauthorized/Unauthorized.js";
 import TodoPage from "./pages/todoPage/todoPage.jsx";
+import NotFound from "./pages/404/404page.js";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         />
         <Route path="/" element={<Home />} />
         <Route path="/unauthorized" element={<Unauthorized />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
